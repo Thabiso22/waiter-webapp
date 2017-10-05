fa426d65f0c43460a9b2fd4af83e58ecb4035424
Thabiso22 <thabisobudaza@gmail.com>
2017-10-05 11:50:38 +0200

    modified

--- index.js
+++ index.js
@@ -54,72 +54,79 @@ app.post("/waiters/:username", function(req, res) {
 ////Show your sister which days waiters can work
 app.get("/days", function(req, res) {
     WaiterShift.find({}, function(err, results) {
-      if (err) {
-        console.log(err);
-      }else {
-        var monday = [];
-        var tuesday = [];
-        var wednesday = [];
-        var thursday = [];
-        var friday = [];
-results.forEach(function (day) {
-  var Name = day.name;
-  var Days = day.days;
-  for (var i = 0; i < Days.length; i++) {
+        if (err) {
+            console.log(err);
+        } else {
+            var monday = [];
+            var tuesday = [];
+            var wednesday = [];
+            var thursday = [];
+            var friday = [];
+            results.forEach(function(day) {
+                var Name = day.name;
+                var Days = day.days;
+                for (var i = 0; i < Days.length; i++) {
 
-    if (Days[i] == "Monday") {
-      monday.push(Name);
-      // console.log("ndinguMonday" + monday);
-    }else if (Days[i] == "Tuesday") {
-      // console.log("ndinguTuesday" + tuesday);
-      tuesday.push(Name);
+                    if (Days[i] == "Monday") {
+                        monday.push(Name);
+                        // console.log("ndinguMonday" + monday);
+                    } else if (Days[i] == "Tuesday") {
+                        // console.log("ndinguTuesday" + tuesday);
+                        tuesday.push(Name);
 
-    }else if (Days[i] == "Wednesday") {
-      wednesday.push(Name);
-      // console.log(wednesday);
-    }else if (Days[i] == "Thursday") {
-      thursday.push(Name);
-      // console.log(thursday);
-    }else if (Days[i] == "Friday") {
-      friday.push(Name);
-      // console.log(friday);
-    }
-
-
-  }
-});
-
-function changeCol(colour)
-{
-  if(colour === 3){return "greenColor";};
-  if (colour < 3) {
-    return "yellowColor";
-  }if (colour > 3){
-    return "redColor";
-  }
-}
-res.render("form2",{
-  mondayDisp:monday,
-  monColor:changeCol(monday.length),
-  tuesdayDisp:tuesday,
-  tueColor:changeCol(tuesday.length),
-  wednesdayDisp:wednesday,
-  wedColor:changeCol(wednesday.length),
-  thursdayDisp:thursday,
-  thursColor:changeCol(thursday.length),
-  fridayDisp:friday,
-  friColor:changeCol(friday.length)
-})
+                    } else if (Days[i] == "Wednesday") {
+                        wednesday.push(Name);
+                        // console.log(wednesday);
+                    } else if (Days[i] == "Thursday") {
+                        thursday.push(Name);
+                        // console.log(thursday);
+                    } else if (Days[i] == "Friday") {
+                        friday.push(Name);
+                        // console.log(friday);
+                    }
 
 
+                }
+            });
 
-
-      }
-
-
-
+            function changeCol(colour) {
+                if (colour === 3) {
+                    return "greenColor";
+                };
+                if (colour < 3) {
+                    return "yellowColor";
+                }
+                if (colour > 3) {
+                    return "redColor";
+                }
+            }
+            res.render("form2", {
+                mondayDisp: monday,
+                monColor: changeCol(monday.length),
+                tuesdayDisp: tuesday,
+                tueColor: changeCol(tuesday.length),
+                wednesdayDisp: wednesday,
+                wedColor: changeCol(wednesday.length),
+                thursdayDisp: thursday,
+                thursColor: changeCol(thursday.length),
+                fridayDisp: friday,
+                friColor: changeCol(friday.length)
+            })
+        }
     })
 });
+
+app.post("/days", function(req, res) {
+    WaiterShift.remove({}, function(err, results) {
+        if (err) {
+            console.log(err);
+        } else {
+            return results;
+        }
+    })
+
+});
+
 app.set("port", (process.env.PORT || 4000));
 app.set("host", (process.env.HOST || "http://localhost"));
 app.listen(app.get("port"), function(err) {
